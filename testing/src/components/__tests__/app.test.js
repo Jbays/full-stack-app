8da582b924dfce59ce2cca50b2db477aad903825
jbays <jjbaize@gmail.com>
2018-06-14 18:20:25 -0500

    Added second test.  Refactored the test code

--- testing/src/components/__tests__/app.test.js
+++ testing/src/components/__tests__/app.test.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+let wrapped;
+beforeEach(()=>{
+  //using wrapped terminology to indicate our App rendered has additional functionality loaded atop.
+  wrapped = shallow(<App />);
+})
 
 it('shows a comment box', ()=>{
-  //using wrapped terminology to indicate our App rendered has additional functionality loaded atop.
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
 
+})
+
+it('shows a comment list', ()=>{
+  expect (wrapped.find(CommentList).length).toEqual(1);
 })
