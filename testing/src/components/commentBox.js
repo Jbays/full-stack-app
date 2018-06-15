249a46f520bc39c951734d2b18ff12576fc0245e
jbays <jjbaize@gmail.com>
2018-06-15 10:50:48 -0500

    Wrote two eventHandlers. Attached to textArea and form elements.

--- testing/src/components/commentBox.js
+++ testing/src/components/commentBox.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = {comment: ''};
+
+
+  //receives event object representing user's typing in textarea
+  handleChange = (event)=>{
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = (event)=>{
+    event.preventDefault();
+
+    //call action creater
+    //and save comment
+    this.setState({ comment:'' });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange}value={this.state.comment}/>
         <div>
           <button>Submit Comment</button>
         </div>
