25aef309360698fcd7fb08771d3ada974d6d9402
jbays <jjbaize@gmail.com>
2018-06-18 14:23:12 -0500

    Adding second text to CommentBox

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -6,9 +6,29 @@ let wrapped;
 
 beforeEach(()=>{
   wrapped = mount(<CommentBox />);
-})
+});
 
 it('has both text area and button',()=>{
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
-})
+});
+
+it('has a text area which accepts text',()=>{
+  // http://airbnb.io/enzyme/docs/api/ReactWrapper/simulate.html
+
+  //'change'.  NOT 'onChange'.
+  wrapped.find('textarea').simulate('change',{
+    target: { value: 'new comment' }
+  });
+
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+//NOTE: mount renders the full DOM.  This might interfere with other parts of our app -- causing problems.
+//better is, after each test, to unmount the wrapped component
+//http://airbnb.io/enzyme/docs/api/ReactWrapper/unmount.html
+afterEach(()=>{
+  wrapped.unmount();
+});
