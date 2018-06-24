18983b642267ef1b14c0372c5c3e2fd7702f31a0
jbays <jjbaize@gmail.com>
2018-06-24 12:29:46 -0500

    Wrote first test for reducers

---
+++ 01-app/src/reducers/__tests__/comments.test.js
@@ -0,0 +1,13 @@
+import commentsReducer from '../comments';
+import { SAVE_COMMENT } from '../../actions/types';
+
+it('handles actions of type "SAVE_COMMENT"',()=>{
+  const action = {
+    type: SAVE_COMMENT,
+    payload: 'This is a new comment'
+  };
+
+  //newState should return an array containing action.payload
+  const newState = commentsReducer([],action);
+  expect(newState).toEqual(['This is a new comment']);
+});
